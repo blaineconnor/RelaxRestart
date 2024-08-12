0a5cc79f05db4bf73f265ce0a88323a5d485d995
blaineconnor <fesarman@anadolu.edu.tr>
2024-08-12 13:38:02 +0300

    Initial Commit

---
+++ RelaxRestart-App/RelaxRestart_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelaxRestart_AppApp.swift
+//  RelaxRestart-App
+//
+//  Created by Fatih Emre Sarman on 12.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct RelaxRestart_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
